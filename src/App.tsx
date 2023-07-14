d8991b1a380b98d808e4dad0ae8f5684302b56d9
tvone <taikhoanx7@gmail.com>
2023-07-14 17:53:08 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import logo from './logo.svg';
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App text-3xl font-bold underline">
+      <p>Hello anh em</p>
+      <p>React App</p>
+    </div>
+  );
+}
+
+export default App;
